9c021b4bb769a9cf4dcf8fa6a49406b9446d1b3f
crazyro15 <crazyro15@hotmail.com>
2022-10-07 18:44:20 -0300

    Calculos basicos listos

---
+++ Display.js
@@ -0,0 +1,53 @@
+class Display{
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '/',
+            multiplicar: '*',
+            restar: '-'
+        }
+    }
+    
+
+    borrar(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero){
+        //Si en el valor actual ya hay un punto, no lo repite.
+        if(numero === '.' && this.valorActual.includes('.')) return
+        //concatena los numeros ingresados en el display.
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+
+    calcular(){
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if(isNaN(valorActual) || isNaN(valorAnterior)) return
+        this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
+    }
+
+    computar(tipo){
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+}
